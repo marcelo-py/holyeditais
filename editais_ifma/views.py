bd483482273dde5e2323b62407e0077aab373bb8
Marcelo Araújo <marcelo.araujo.999s@gmail.com>
2022-06-29 22:16:59 -0300

    otimizando a aplicação para por em produção

--- editais_ifma/views.py
+++ editais_ifma/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from json import load
 from .models import Menssagens, FormMenssage, Edital
+import subprocess
+from subprocess import PIPE
+import datetime
 
-
+hora = datetime.datetime.today().hour
 def index(request):
     form = FormMenssage()
+    print(hora)
+    if hora in (7, 8, 23, 0, 1, 2):
+        subprocess.run('scrapy runspider editais.py -O edital.json', stderr=PIPE, stdout=PIPE)
 
     if request.method != 'POST':
         with open('edital.json', 'r', encoding='utf8') as file:
